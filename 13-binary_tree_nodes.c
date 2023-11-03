6795328ee9c369418486c08de73603316bf01b45
Andrew <andrewsolitei@gmail.com>
2023-11-03 15:37:43 +0300

    child nodes

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - Counts the nodes with at least 1 child in a binary tree
+ * @tree: Pointer to the root node of the tree to measure the size
+ *
+ * Return: Size of the tree
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t nodes = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left != NULL || tree->right != NULL)
+	{
+		/* Subtract one for the parent node */
+		nodes++;
+	}
+
+	if (tree->left != NULL)
+	{
+		nodes += binary_tree_nodes(tree->left);
+	}
+
+	if (tree->right != NULL)
+	{
+		nodes += binary_tree_nodes(tree->right);
+	}
+
+	return (nodes);
+}
